9bb6238a337f4dcef7ca9aa3285ede0743f39fe1
KHELILI Akram <43978087+akram92i1@users.noreply.github.com>
2021-04-21 17:23:56 -0500

    Update index.js

--- src/index.js
+++ src/index.js
@@ -3,7 +3,7 @@ import ReactDOM from 'react-dom';
 import './index.css';
 import App from './containers/App';
 import registerServiceWorker from './registerServiceWorker';
-
+// hello
 
 ReactDOM.render(<App appTitle="Person Manager" />, document.getElementById('root'));
 registerServiceWorker();
